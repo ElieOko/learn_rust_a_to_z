3fb3b18fd6d45fff8a92abfaea96c694e376636d
elieoko <elieoko100@gmail.com>
2024-03-10 19:11:36 +0100

    feat : Implementation Operator

---
+++ src/operator.rs
@@ -0,0 +1,21 @@
+fn main() {
+    //Operateur Arithmétique
+    let _a = 6 + 4;
+    let _b = _a - 4;
+    let _c = _b * 2;
+    let _d = _c / 2;
+    let _e = 18 % 2;
+    //Operateur de comparaison
+    let _is_large = 12 > 2; //true
+    let _is_small = 6 < 10; //true
+    let _is_equal = 9 == 8; //false
+    let _is_noequal = 15 != 12; //true
+    let _is_small_or_equal = 6 <= 12; //true
+    let _is_large_or_equal = 12 >= 12; /*true */
+    //*Operateur d'affectation
+    let _test = true;
+    //Operateur Logique
+    let _is_way = !_test; //false
+    let _is_access = true == !_is_way && true != false; //true
+    let _is_actif = _is_access != false || !_test == true; //true
+}
