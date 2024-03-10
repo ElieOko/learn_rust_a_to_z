a68aacf1e792ec7dd294922aaa2c2361ca00b103
elieoko <elieoko100@gmail.com>
2024-03-10 18:41:07 +0100

    feat : implementation function

---
+++ src/function.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut name = String::new();
+    let _a = 12;
+    let _b = 13;
+    let result = addition(_a, _b);
+    println!("{0} + {1} = {2}", _a, _b, result);
+    println!("Writte your name please:");
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("Exception detecté");
+    greeting(&name);
+}
+
+//Fonction retournant une valeur en retour
+fn addition(n1: i32, n2: i32) -> i32 {
+    n1 + n2
+}
+
+//fonction ne retournant pas de valeur avec un parametre de reference
+fn greeting(name: &str) {
+    // let x = side_effect;
+    println!("Hi {}", name);
+}
